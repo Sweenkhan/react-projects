30ec88af9fc8aefc13bbbd2230d110428c2bf406
sween <khansween@gmail.com>
2023-09-03 23:52:42 +0530

    first interviewd questio solved

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Portal from './portal/Portal';
+import App from "./interview/App"
+// import Portal from './portal/Portal';
 // import App from "./checkyCity/App"
 // import App from "./useEffect/App"
 // import App from "./netflix/App"
@@ -15,7 +16,7 @@ import Portal from './portal/Portal';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( 
-    <Portal /> 
+    <App /> 
 );
 
  
---
+++ src/interview/App.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+
+function App() {
+
+    const contries = [
+        {name: "India", code: "IN", city: ["new delhi", "mumbai"]},
+        {name: "Pakistan", code: "PK", city: ["karachi", "lahore"]},
+        {name: "Bangladesh", code: "BG", city: ["daka", "imphal"]}
+    ]
+
+
+    const [selectedValue, setSelectedValue] = useState("")
+    // const [index, setIndex] = useState(0)
+
+ console.log(selectedValue)
+    // function handleInput(e) {
+    //     e.preventDefault();
+    //        console.log(e.target.value)
+    //        setIndex(index)
+    //        console.log("how are u")
+    // }
+
+  return (
+    <div className='container'>
+        <h2>Choose anyone</h2>
+
+        <select onChange={e => setSelectedValue(e.target.value)}  defaultValue="placeholder" > 
+        <option value="placeholder" disabled>Choose contry</option>
+        {
+            contries.map((contry, index) => {
+                return <option key={index}  value={index}>{contry.name}</option>
+            })
+        }
+        </select>
+
+        {/* chosse city */}
+            <select>
+        {
+         selectedValue &&   contries[selectedValue].city.map((citi, index) =>{
+             return <option key={index}>{citi}</option>
+           })
+        }
+            </select>
+    </div>
+  )
+}
+
+export default App
