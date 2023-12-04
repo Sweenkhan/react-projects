0d58fda40d548d148d1212486ab8b5ab36d5bec7
Sween Khan <136142278+Sweenkhan@users.noreply.github.com>
2023-12-04 22:14:03 +0530

    Update App

--- src/axios/App.jsx
+++ src/axios/App.jsx
@@ -10,7 +10,6 @@ function App() {
  
     function getRandomData() {
           
-     
         axios.get('https://randomuser.me/api/')
         .then((response) => {
             console.log(response)
